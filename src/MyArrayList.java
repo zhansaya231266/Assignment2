121a653dc720d1e64c68bfc5624bed206e1772bb
User <231266@astanait.edu.kz>
2024-04-24 22:27:43 +0500

    assi 2

--- src/MyArrayList.java
+++ src/MyArrayList.java
@@ -1,7 +1,70 @@
-interface Mylist{
-
-}
-public class MyArrayList implements Mylist{
+public class MyArrayList<T> implements Mylist<T> {
     private Object[] elements;
     private int size;
+
+    public MyArrayList(Object[] elements, int size) {
+        this.elements = (T[]) new Object[5];
+        this.size = 0;
+    }
+
+    @Override
+    public void add(T element) {
+        if(size >= elements.length) {
+            increaseBuffer();
+        }
+        elements[size++] = element;
+    }
+
+    public void add(T element, int index) {
+        if(size >= elements.length) {
+            increaseBuffer();
+        }
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is not correct");
+        }
+        elements[index] = element;
+    }
+
+    private void increaseBuffer() {
+        T[] newElements = (T[]) new Object[elements.length * 2];
+        for(int i = 0; i < size; i++) {
+            newElements = elements[i];
+        }
+        elements = newElements;
+    }
+
+    public T getElem(int index) {
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is not correct");
+        }
+        return elements[index];
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printElems() {
+        for(int i = 0; i < size; i++) {
+            System.out.println(elements[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void remove(int index) {
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index is not correct");
+        }
+        for(int i = index + 1; i < size; i++) {
+            elements[i - 1] = elements[i];
+        }
+        size--;
+    }
+
+    public void clear() {
+        elements = (T[] new Object[5]);
+        size = 0;
+    }
+
 }
+
