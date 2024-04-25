cd1a3f6b4e9768a3b094b1baad1b3b19b75d4ef9
User <231266@astanait.edu.kz>
2024-04-25 21:27:59 +0500

    assi 2

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,2 +1,21 @@
-public class MyLinkedList {
+public class MyLinkedList<E> implements MyList<E> {
+    private class MyNode<E> {
+        E data;
+        MyNode<E> next;
+        MyNode<E> prev;
+
+        public MyNode(E data) {
+            this.data = data;
+            next = null;
+            prev = null;
+        }
+    }
+
+    private MyNode head;
+    private MyNode tail;
+    private int size;
+
+    public MyLinkedList() {
+
+    }
 }
